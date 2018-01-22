cc55bbb26e6d3eb5ac53ecb0e6586028cfc1988a
Why-Amazing <wocwacc@outlook.com>
2018-01-22 21:28:16 +0800

    first commit

---
+++ src/com/chapter_01/Test.java
@@ -0,0 +1,12 @@
+package com.chapter_01;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		double[] a = {0.1,0.3,0.3,0.3};
+		int result = StdRandom.discrete(a);
+		System.out.println(result);
+	}
+}
